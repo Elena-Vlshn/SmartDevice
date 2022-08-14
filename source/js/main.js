760cf871359d10809292c51901a1de2c7478af52
Elena Voloshina <elen3011@yandex.ru>
2022-08-14 18:00:59 -0400

    реализованы аккордеон и модальное окно с формой

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,63 @@ import {initModals} from './modules/modals/init-modals';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
+  const callButton = document.querySelector('.header__button');
+  const modal = document.querySelector('.modal');
+  const body = document.querySelector('.body');
+  const modalForm = modal.querySelector('.form');
+  // const modalFormSubmitButton = modal.querySelector('button[type="submit"]');
+
+  const accordionButtons = document.querySelectorAll('.accordion__button');
+  const accordions = document.querySelectorAll('.accordion');
+
+  for (let accordion of accordions) {
+    accordion.querySelector('.accordion__body').classList.add('accordion__body_closed');
+    accordion.querySelector('.accordion__button').classList.remove('accordion__button_hidden');
+  }
+
+  function outsideEvtListener(event) {
+    const target = event.target;
+    if (target.classList.contains('modal__overlay')) {
+      modal.classList.remove('modal_opened');
+      body.classList.remove('scroll-lock');
+      document.removeEventListener('click', outsideEvtListener);
+      modalForm.reset();
+    }
+  }
+
+  function modalClose() {
+    modal.classList.remove('modal_opened');
+    body.classList.remove('scroll-lock');
+    document.removeEventListener('click', outsideEvtListener);
+    modalForm.reset();
+  }
+
+
+  callButton.addEventListener('click', function () {
+    const modalClosedButton = modal.querySelector('.modal__close-btn');
+    modal.classList.add('modal_opened');
+    body.classList.add('scroll-lock');
+    document.addEventListener('click', outsideEvtListener);
+    modalClosedButton.addEventListener('click', modalClose);
+    // modalFormSubmitButton.addEventListener('click', modalClosed);
+  });
+
+  for (let button of accordionButtons) {
+    button.addEventListener('click', function (event) {
+      const accordionBody = event.target.closest('.accordion').querySelector('.accordion__body');
+      if (button.classList.contains('accordion__button_closed')) {
+        button.classList.toggle('accordion__button_closed');
+        accordionBody.classList.toggle('accordion__body_closed');
+      } else {
+        for (let elem of accordions) {
+          elem.querySelector('.accordion__body').classList.add('accordion__body_closed');
+          elem.querySelector('.accordion__button').classList.remove('accordion__button_closed');
+        }
+        accordionBody.classList.toggle('accordion__body_closed');
+        button.classList.toggle('accordion__button_closed');
+      }
+    });
+  }
 
   // Utils
   // ---------------------------------
